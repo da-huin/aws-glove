fbc9984e622b7ea8e3d0317166042b116db1b37b
jaden <jaden@kmong.com>
2021-03-08 04:05:47 +0900

    배포 테스트 2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 from setuptools import find_packages, setup
 
 setup(name='aws_glove',
-      version='0.0.2',
+      version='0.0.3',
       description='AWS를 쉽게 사용할 수 있게 도와줍니다.',
       long_description="Please refer to the https://github.com/jaden-git/aws_glove",
       long_description_content_type="text/markdown",
